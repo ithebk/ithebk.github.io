2a4b279adf7fdb569440208df0d070ce2cae96a8
Bharath Kumar <bharath.kumar@autoninja.in>
2017-10-07 22:14:38 +0530

    UI Changed and Projects Added with MDL Grid

---
+++ js/main.js
@@ -0,0 +1,28 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCMzIv6MdPya32XeOqolUiAbTFiqZX5QG4",
+    authDomain: "healthy-battery-160212.firebaseapp.com",
+    databaseURL: "https://healthy-battery-160212.firebaseio.com",
+    projectId: "healthy-battery-160212",
+    storageBucket: "healthy-battery-160212.appspot.com",
+    messagingSenderId: "1064128468443"
+  };
+  firebase.initializeApp(config);
+
+
+
+<div class="demo-card-square mdl-card mdl-shadow--1dp">
+  <div class="mdl-card__title mdl-card--expand">
+    <h2 class="mdl-card__title-text">Hello world</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Aenan convallis.
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      View Updates
+    </a>
+  </div>
+</div>
+
